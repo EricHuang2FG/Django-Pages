0069e8d55d2adf5dd668cc2f155fdbe94ce38563
EricHuang2FG <xilahuade@foxmail.com>
2024-03-16 00:04:02 -0400

    Adding a time page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, TimePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name = 'home'),
     path("about/", AboutPageView.as_view(), name = 'about'),
+    path("time/", TimePageView.as_view(), name = 'time'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+class TimePageView(TemplateView):
+    template_name = "time.html"
